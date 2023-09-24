14bef3b38ee13575583060c1da16a38600bc2aab
Jonathan Gilbert <jpg@trillica.com>
2023-09-24 13:43:58 +1000

    menu trigger fixes

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,5 @@
 function toggleMenu() {
-    var bar = document.getElementById("sidebar");
-
-    var menu = bar.getElementsByTagName("nav")[0];
-
+    var menu = document.getElementById("sidebar").getElementsByTagName("nav")[0];
     if(menu.style.left == "") {
         menu.style.left = "-12em";
     } else if (menu.style.left = "-12em") {
@@ -12,5 +9,9 @@ function toggleMenu() {
 
 window.onload = function(e){
     document.getElementById("navtrigger").addEventListener("touchend", toggleMenu, false);
+    var items = document.getElementsByClassName("s-sidebar__trigger");
+    for (var i = 0; i < items.length; i++) {
+      items[i].addEventListener("touchend", toggleMenu, false);
+    }
 }
 
